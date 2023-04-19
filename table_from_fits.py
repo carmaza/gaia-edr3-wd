e7fc6eb6de2a6e280899c6735b9ba00d9644d0d2
Cristóbal Armaza <ca455@cornell.edu>
2023-04-19 00:36:57 -0400

    Return table from fits - Also document function and run YAPF on file

--- table_from_fits.py
+++ table_from_fits.py
@@ -1,16 +1,35 @@
+# Distributed under the MIT License.
+# See LICENSE for details.
+"""
+Defines function :func:`.table_from_fits`.
+
+"""
+
 import warnings
 
 from astropy.io import fits
 from astropy.table import Table
 
 
-def read(path="GaiaEDR3_WD_main.fits"):
+def table_from_fits(path="GaiaEDR3_WD_main.fits"):
     """
-    Read catalogue of white dwarfs in Gaia EDR3.
-    N. P. Gentille Fusillo et al. 2021 (GF+21)
+    Read catalogue of white dwarfs in Gaia EDR3 and return table.
 
+    N. P. Gentille Fusillo et al. 2021 (GF+21)
     MNRAS 508, 3877–3896 (2021)
     https://doi.org/10.1093/mnras/stab2672
+
+    Parameters
+    ----------
+
+    path : str
+        The path to the catalogue, including filename and FITS extension.
+
+    Returns
+    -------
+
+    out : astropy.table.table.Table
+        The catalogue as a table.
 
     """
 
@@ -23,15 +42,13 @@ def read(path="GaiaEDR3_WD_main.fits"):
     print(f"Opened file {path}.")
 
     # Creating a new table from the catalogue provided by GF+21 throws some
-    # `UnitsWarning`s associated to units impossible to parse as fits units. 
+    # `UnitsWarning`s associated to units impossible to parse as fits units.
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
 
         print("Creating table from catalogue...")
-        
+
         # 'astropy.table.table.Table' object.
         table = Table.read(bintable)
 
-
-if __name__ == "__main__":
-    read()
+    return table
